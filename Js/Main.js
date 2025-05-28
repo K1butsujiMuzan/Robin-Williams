e3daa1d04a75fa357bc69919c4c24468fc30fa30
K1butsujiMuzan <maks_shishkin_2007@inbox.ru>
2025-05-28 18:39:25 +0200

    First commit

---
+++ Js/Main.js
@@ -0,0 +1,31 @@
+"use strict";
+const BurgerMenuLinks = document.querySelector(".ModalBurgerMenu");
+let BurgerisOpen = false;
+
+function CheckBurgerMenu(){
+    if(BurgerisOpen == false) {
+        OpenBurgerMenu();      
+        BurgerisOpen == true;
+    } else{
+        CloseBurgerMenu();        
+        BurgerisOpen == false;
+    }
+}
+
+function OpenBurgerMenu() {
+    BurgerMenuLinks.classList.toggle("Active");  
+}
+
+function CloseBurgerMenu() {   
+    BurgerMenuLinks.classList.toggle("Active");   
+}
+BurgerMenuLinks.addEventListener("click", function(e) {
+    if(e.target === BurgerMenuLinks)
+    {
+        CloseBurgerMenu();
+    }
+});
+
+if(window.innerWidth > 768) {
+    BurgerMenuLinks.classList.remove("Active");
+}
